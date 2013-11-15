61b70093b2c5e522adb6717a670da80f1fbe79cc
Raghunath J <jrcbangalore@gmail.com>
2013-11-15 10:08:30 +0530

    Added Mobile Touch events
    No Errors and working fine on iPad

--- slider.js
+++ slider.js
@@ -15,7 +15,10 @@
 			"spyactiveclass":"spyactive",
 			"spycontaineroverwrite":true,
 			"activeclass":"active",
-			"debugmode":true
+			"debugmode":true,
+			"touchminx":20,
+			"touchminy":20,
+			"touchswipe":true
 		}
 
 		if (settings){
@@ -60,6 +63,75 @@
 					e.preventDefault();
 					stopSlideShow();
 				});
+
+				//Code for handeling touchdevice
+				var sx,sy;
+				var sliding = false;
+
+				if(options.touchswipe){
+					this.addEventListener('touchstart',onTouch, false);	
+				}
+    	 		
+    		 	function removeTouch(){
+    		 		this.removeEventListener('touchmove', onTouchMove);
+		    		sx = null;
+		    		sliding = false;
+    		 	}
+
+    	 		function onTouch(e){
+    	 			debugMessage("User is using mobile device and doing touch event");
+    	 			if (e.touches.length == 1) {
+    	 				sx = e.touches[0].pageX;
+    	 				sy = e.touches[0].pageY;
+    	 				sliding = true;
+    	 				this.addEventListener('touchmove',onTouchMove, false);
+    	 			}
+    	 		}
+
+    	 		function onTouchMove(e){
+    	 			e.preventDefault();
+    	 			if(sliding){
+    	 				 var x = e.touches[0].pageX;
+			    		 var y = e.touches[0].pageY;
+			    		 var dx = sx - x;
+			    		 var dy = sy - y;
+			    		 if(Math.abs(dx) >= options.touchminx) {
+			    		 	removeTouch();
+			    		 	if(dx > 0){
+			    		 		touchaction("left");
+			    		 	}else{
+			    		 		touchaction("right");
+			    		 	}
+			    		 }else if(Math.abs(dy) >= options.touchminy){
+			    		 	removeTouch();
+			    		 	if(dy > 0) {
+		    					touchaction("down");
+			    			}
+			    			else {
+			    				touchaction("up");
+			    			}
+			    		 }
+    	 			}
+    	 		}
+
+    	 		function touchaction(action){
+    	 			switch(action){
+    	 				case "left":
+    	 					debugMessage("Touch Left");
+    	 					slide("next");
+    	 				break;
+    	 				case "right":
+    	 					debugMessage("Touch Right");
+    	 					slide("prev");
+    	 				break;
+    	 				case "up":
+    	 					debugMessage("Touch Up");
+    	 				break;
+    	 				case "down":
+    	 					debugMessage("Touch Down");
+    	 				break;
+    	 			}
+    	 		}
 
 				//On hover
 				child.hover(function(){
